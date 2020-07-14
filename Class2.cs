e06408e73bb5bd0f07911327c22dbce5f86586fb
FelipeGaviria <68086818+FelipeGaviria@users.noreply.github.com>
2020-07-14 13:56:06 -0500

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		Console.write ("Inserte estatura(m): ");
+
+		double estatura = double.Parse(Console.ReadLine());
+
+		Console.write("Inserte peso (kg): ");
+
+		double peso = double.Parse(Console.ReadLine());
+
+		double Indice = (peso / (estatura*estatura));
+
+		if(Indice < 18.5){
+			Console.writeLine("Bajo peso");
+		}
+		else if(18.5 < Indice && Indice < 24.9 ){
+			Console.writeLine("Normal");
+		}
+		else if (25 < Indice && Indice < 29.9)
+		{
+			Console.writeLine("Sobrepeso");
+		}
+		if (Indice < 30)
+		{
+			Console.writeLine("Sobrepeso");
+		}
+	}
+}
---
+++ Codigo Discriminante.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Fórmula_cuadrática
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingresa a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double discriminante = ((b * b) - 4 * a * c);
+
+
+            if (discriminante == 0)
+            {
+                double discrimCero = -b / (a * 2);
+                Console.WriteLine("Tiene solucion única: " + discrimCero);
+            }
+            else if (discriminante > 0)
+            {
+                double formuCua = (-b + Math.Sqrt(discrim)) / (a * 2);
+                double formuNeg = (-b - Math.Sqrt(discrim)) / (a * 2);
+                Console.WriteLine("tiene dos soluciones. X1: " + formuCua);
+                Console.WriteLine("X2: " + formuNeg);
+            }
+            else
+            {
+                Console.WriteLine("No tiene solución.");
+            }
+        }
+    }
+}
