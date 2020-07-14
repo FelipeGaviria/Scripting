4baa51f0411778d1aad689fa933b3e71a28aae38
FelipeGaviria <68086818+FelipeGaviria@users.noreply.github.com>
2020-07-14 13:48:44 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,36 @@
-﻿
-using System;
-//Trangulo programa 3
+﻿using System;
+
 public class Program
 {
-    public static void Main()
-    {
-        // Entrada del usuario de las variables
-        Console.Write("Ingrese w: ");
-        double w = double.Parse(Console.ReadLine());
+	public static void Main()
+	{
 
-        Console.Write("Ingrese d: ");
-        double d = double.Parse(Console.ReadLine());
+		Console.Write("Inserte estatura(m): ");
 
-        Console.Write("Ingrese x: ");
-        double x = double.Parse(Console.ReadLine());
-        //Convertir d a rads
-        double drad = d / (180.0 / Math.PI);
-        // Calculos
-        double z = Math.Sin(drad) * w;
-        double xy = Math.Sqrt(((w)*(w)) - ((z)*(z)));
-        double y = xy - x;
-        //Resultado
-        Console.WriteLine("y= " + y);
-    }
+		double estatura = double.Parse(Console.ReadLine());
+
+		Console.Write("Inserte peso (kg): ");
+
+		double peso = double.Parse(Console.ReadLine());
+
+		double Indice = (peso / (estatura * estatura));
+
+		if (Indice < 18.5)
+		{
+			Console.WriteLine("Bajo peso");
+		}
+		else if (18.5 < Indice && Indice < 24.9)
+		{
+			Console.WriteLine("Normal");
+		}
+		else if (25 < Indice && Indice < 29.9)
+		{
+			Console.WriteLine("Sobrepeso");
+		}
+		if (Indice < 30)
+		{
+			Console.WriteLine("Sobrepeso");
+		}
+
+	}
 }
