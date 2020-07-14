809fa8c7d803cca452655b7becbfbc04c4297724
FelipeGaviria <68086818+FelipeGaviria@users.noreply.github.com>
2020-07-14 11:08:57 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+//Trangulo programa 4
+public class Program
+{
+    public static void Main()
+    {
+        // Entrada del usuario de las variables
+        Console.Write("Ingrese w: ");
+        double w = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese t: ");
+        double t = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese c: ");
+        double c = double.Parse(Console.ReadLine());
+        // Calculos 1
+        double e = (180 - c);
+        //Convertir e a rads
+        double erad = e / (180.0 / Math.PI);
+        // Calculos 2
+        double drad = Math.Asin((Math.Sin(erad) * (t)) / w);
+        //Convertir drad a grados
+        double d = drad * (180.0 / Math.PI);
+        // Calculos 3
+        double b = (180.0 - (d + e));
+        //Convertir b a rads
+        double brad = b / (180.0 / Math.PI);
+        // Calculo Final
+        double x = (Math.Sin(brad) * (t)) / Math.Sin(drad);
+        //Resultado
+        Console.WriteLine("x= " + x);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+//Trangulo programa 3
+public class Program
+{
+    public static void Main()
+    {
+        // Entrada del usuario de las variables
+        Console.Write("Ingrese w: ");
+        double w = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese d: ");
+        double d = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese x: ");
+        double x = double.Parse(Console.ReadLine());
+        //Convertir d a rads
+        double drad = d / (180.0 / Math.PI);
+        // Calculos
+        double z = Math.Sin(drad) * w;
+        double xy = Math.Sqrt(((w)*(w)) - ((z)*(z)));
+        double y = xy - x;
+        //Resultado
+        Console.WriteLine("y= " + y);
+    }
+}
